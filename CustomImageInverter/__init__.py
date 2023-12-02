64536c4ae382129b32b52555f67be2476f16d091
ivanz <ivan.zhdanov@hotmail.com>
2023-12-02 21:44:29 +0100

    feat: Simple test

---
+++ CustomImageInverter/__init__.py
@@ -0,0 +1,5 @@
+from .main import CustomImageInverter
+
+NODE_CLASS_MAPPINGS = {"RedBeanie_CustomImageInverter": CustomImageInverter}
+NODE_DISPLAY_NAME_MAPPINGS = {"RedBeanie_CustomImageInverter": "Image Inverter"}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
